05db6dc41313ddc446081b9512c8d6249c448f88
AAbdi2 <aabdi_2@yahoo.com>
2023-02-09 10:12:01 +0000

    Wk3Day1

--- src/main/java/CauseUncheckedException.java
+++ src/main/java/CauseUncheckedException.java
@@ -15,5 +15,10 @@ public class CauseUncheckedException {
      */
     public void throwUncheckedException(){
 
+        int a = 12;
+        int b = 0;
+        
+        System.out.println(a / b);
+    
     }
 }
